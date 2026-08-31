f67716ea6c6cc67fe6dfc8ceba4b3883a3b6e40b
Jaje11418 <jjtaveram@gmail.com>
2026-08-31 10:38:55 -0600

    Actualizar practica_1.java
    cambio de class

--- practica_1.java
+++ practica_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class P1 {
+public class practica_1{
     public static void main(String[] args) {
        System.out.println("Calculadora");
        Scanner scan = new Scanner(System.in);
