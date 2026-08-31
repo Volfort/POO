4a6a1749a17b773f0eee494526a7780fb3a39e56
Jaje11418 <jjtaveram@gmail.com>
2026-08-31 10:42:44 -0600

    Actualizar practica_1.java

--- practica_1.java
+++ practica_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class practica_1{
+public class Practica_1{
     public static void main(String[] args) {
        System.out.println("Calculadora");
        Scanner scan = new Scanner(System.in);
