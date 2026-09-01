07bbc05b0b8df748086470cdb5701c77e121f6f8
JGio-DF <jesusflores272007@gmail.com>
2026-09-01 15:27:36 -0600

    Añadido el ejercicio número 3
    n       n*10        n*100      n*1000
    1         10           100           1000
    1         10           100           1000

---
+++ Ejer3.java
@@ -0,0 +1,10 @@
+public class Ejer3{
+    public static void main(String[] args){
+        System.out.println("n\tn*10\tn*100\tn*1000");
+        int i = 1;
+        while (i < 6){
+            System.out.println(i+"\t "+ i*10 +"\t "+ i*100+"\t "+i*1000);
+            i++;
+        }
+    }
+}
